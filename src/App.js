e78e0b0823768dbe0b938e1b7b728ed21c96a5a2
liminjun <liminjun@cmss.chinamobile.com>
2019-09-10 18:08:19 +0800

    feat:添加Person组件

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>hi I am a React App.</h1>
+        <p>this is really working.</p>
+        <Person></Person>
       </div>
+     
     );
+    //return React.createElement('div',{className:'App'},'h1','I am React Developer.');
   }
 }
 

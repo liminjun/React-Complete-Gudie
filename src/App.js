01e8118c45ca0bd5963b43a8b8083987f24e2305
liminjun <v-leeli@hotmail.com>
2019-09-15 15:08:06 +0800

    feat:passing method ref between components.

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     ],
     otherState: 'some other value.'
   };
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('was clicked');
     //this.state.persons[0].name="name changed."
     this.setState({
       persons: [
-        { name: "liminjun", age: 30 },
+        { name: newName, age: 30 },
         { name: "xing", age: 30 },
         { name: "wangjian", age: 30 }
       ]
@@ -29,8 +29,11 @@ class App extends Component {
       <div className="App">
         <h1>hi I am a React App.</h1>
         <p>this is really working.</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Reading</Person>
+        <button onClick={()=>this.switchNameHandler('udemy')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this,'udacity')}>Reading</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Playing game.</Person>
       </div>

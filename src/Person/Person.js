12fe6d4b5f1b39d1f6aadb2b9ddef8919f953729
liminjun <v-leeli@hotmail.com>
2019-09-19 21:39:25 +0800

    feat:testing GitHub actions.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css';
 const person = (props) => {
     return (
         <div className="Person">
-  
+            <h3>hello world.</h3>
             <p onClick={props.click}> I am {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
